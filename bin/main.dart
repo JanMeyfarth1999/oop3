4d6668b471948eaa7539e68c1b1312685bfd5ff1
JanMeyfarth1999 <jan_meyfarth@teilnehmer.cdemy.de>
2026-09-23 11:27:54 +0200

    uwe

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,13 @@
 import 'package:oop3/triangle.dart';
 
 void main() {
-  Triangle triangle1 = Triangle.feet(2, 3);
+  Triangle triangle1 = Triangle.cm(100, 200);
+  
    
-  print(triangle1.width);
-  print(triangle1.height);
+  print(triangle1.widthInMeters);
+  print(triangle1.heightInMeters);
+
+  triangle1.widthInMeters = 3;
+
+  print(triangle1.widthInFeet);
 }
--- lib/triangle.dart
+++ lib/triangle.dart
@@ -41,29 +41,101 @@ class Triangle {
         _convertToMm(height, MeasurementSystem.feet),
       );
 
-    double get widthInMillimeters {
-        return _convertFromMm(_width, MeasurementSystem.mm);
-    }
+  double get widthInMillimeters {
+    return _convertFromMm(_width, MeasurementSystem.mm);
+  }
 
-    set widthInMillimeters (double value) {
-        _width = _convertToMm(value, MeasurementSystem.mm);
-    }
-    
-    double get widthInCentimeters {  
-        return _convertFromMm(_width, MeasurementSystem.cm);
-    }
+  set widthInMillimeters(double value) {
+    _width = _convertToMm(value, MeasurementSystem.mm);
+  }
 
-    set widthInCentimeters (double value) {
-        _width = _convertToMm(value, MeasurementSystem.cm);
-    }
-    
-    double get widthInMeters {
-        return _convertFromMm(_width, MeasurementSystem.m);
-    }
+  double get widthInCentimeters {
+    return _convertFromMm(_width, MeasurementSystem.cm);
+  }
 
-    set widthInMeters (double value) {
-        _width = _convertToMm(value, MeasurementSystem.m);
-    }
+  set widthInCentimeters(double value) {
+    _width = _convertToMm(value, MeasurementSystem.cm);
+  }
+
+  double get widthInDecimeters {
+    return _convertFromMm(_width, MeasurementSystem.dm);
+  }
+
+  set widthInDecimeters(double value) {
+    _width = _convertToMm(value, MeasurementSystem.dm);
+  }
+
+  double get widthInMeters {
+    return _convertFromMm(_width, MeasurementSystem.m);
+  }
+
+  set widthInMeters(double value) {
+    _width = _convertToMm(value, MeasurementSystem.m);
+  }
+
+  double get widthInInches {
+    return _convertFromMm(_width, MeasurementSystem.inch);
+  }
+
+  set widthInInches(double value) {
+    _width = _convertToMm(value, MeasurementSystem.inch);
+  }
+
+  double get widthInFeet {
+    return _convertFromMm(_width, MeasurementSystem.feet);
+  }
+
+  set widthInFeet(double value) {
+    _width = _convertToMm(value, MeasurementSystem.feet);
+  }
+
+  double get heightInMillimeters {
+    return _convertFromMm(_height, MeasurementSystem.mm);
+  }
+
+  set heightInMillimeters(double value) {
+    _height = _convertToMm(value, MeasurementSystem.mm);
+  }
+
+  double get heightInCentimeters {
+    return _convertFromMm(_height, MeasurementSystem.cm);
+  }
+
+  set heightInCentimeters(double value) {
+    _height = _convertToMm(value, MeasurementSystem.cm);
+  }
+
+  double get heightInDecimeters {
+    return _convertFromMm(_height, MeasurementSystem.dm);
+  }
+
+  set heightInDecimeters(double value) {
+    _height = _convertToMm(value, MeasurementSystem.dm);
+  }
+
+  double get heightInMeters {
+    return _convertFromMm(_height, MeasurementSystem.m);
+  }
+
+  set heightInMeters(double value) {
+    _height = _convertToMm(value, MeasurementSystem.m);
+  }
+
+  double get heightInInches {
+    return _convertFromMm(_height, MeasurementSystem.inch);
+  }
+
+  set heightInInches(double value) {
+    _height = _convertToMm(value, MeasurementSystem.inch);
+  }
+
+  double get heightInFeet {
+    return _convertFromMm(_height, MeasurementSystem.feet);
+  }
+
+  set heightInFeet(double value) {
+    _height = _convertToMm(value, MeasurementSystem.feet);
+  }
 
   static double _convertToMm(double value, MeasurementSystem system) {
     return value * system.factor;
